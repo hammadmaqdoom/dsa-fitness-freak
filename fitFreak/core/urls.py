45933690b8d9031ec78b7542d6a6c418ee934ab6
Hammad Maqdoom <hammadmaqdoom1@gmail.com>
2020-11-13 17:46:03 +0500

    added new views

--- fitFreak/core/urls.py
+++ fitFreak/core/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('about', views.about, name='about'),
+    path('signup', views.signup, name='signup'),
+    path('contact', views.contact, name='contact'),
+    path('food', views.food, name='food'),
+    path('exercise', views.exercise, name='exercise'),
 ]
--- fitFreak/core/views.py
+++ fitFreak/core/views.py
@@ -3,4 +3,23 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request,"index.html")
+    return render(request,"index.html", {'type': 'index-page'})
+
+def about(request):
+    return render(request,"about.html", {'type': 'index-page'})
+
+def login(request):
+    return render(request,"login.html", {'type': 'register-page'})
+
+def signup(request):
+    return render(request,"signup.html", {'type': 'register-page'})
+
+def food(request):
+    return render(request,"food.html", {'type': 'index-page'})
+
+def exercise(request):
+    return render(request,"exercise.html", {'type': 'index-page'})
+
+def contact(request):
+    return render(request,"contact.html", {'type': 'profile-page'})
+    
